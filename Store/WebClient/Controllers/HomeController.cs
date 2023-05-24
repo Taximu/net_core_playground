8e2fc8e479811f2072c913ffc495b9385ca21908
Taximu <maxim_gorbunov@epam.com>
2023-05-24 22:35:52 +0300

    Added asp.net core mvc project and oidc

---
+++ Store/WebClient/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebClient.Models;
+
+namespace WebClient.Controllers;
+
+public class HomeController : Controller
+{
+    //[Authorize(Roles="Manager")]
+    [Authorize(Roles="Buyer")]
+    public async Task<IActionResult> Index()
+    {
+        var result = "Hello!";
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.GetAsync("http://localhost:5019/api/v1/categories"))
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+        }
+        return new OkObjectResult(result);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
